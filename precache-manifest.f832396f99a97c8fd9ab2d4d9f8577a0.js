d35d10787d0ed702d0e9979542e75a50e048d062
RofixWork <wourkout123@gmail.com>
2020-10-07 14:14:29 +0100

    Updates

---
+++ precache-manifest.f832396f99a97c8fd9ab2d4d9f8577a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3633521b103536f1473d9cbd9c6ca2e7",
+    "url": "/websitepizza/index.html"
+  },
+  {
+    "revision": "b7f24e4fc8aeedc85df9",
+    "url": "/websitepizza/static/css/main.8f85d94b.chunk.css"
+  },
+  {
+    "revision": "24e9ff815348eb7b8511",
+    "url": "/websitepizza/static/js/2.384a2b70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/websitepizza/static/js/2.384a2b70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f24e4fc8aeedc85df9",
+    "url": "/websitepizza/static/js/main.2a3c36d9.chunk.js"
+  },
+  {
+    "revision": "d42ba6dfd7b5ce66bc06",
+    "url": "/websitepizza/static/js/runtime-main.f37359fd.js"
+  }
+]);
